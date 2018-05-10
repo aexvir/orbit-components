7b611e4e2249d52471bfb0807a82b90cc3512426
Tomáš Hapl <tomas.hapl@gmail.com>
2018-05-10 14:02:56 +0200

    Created deprecated folder

--- src/index.js
+++ src/index.js
@@ -3,22 +3,13 @@ import defaultTheme from "@kiwicom/orbit-design-tokens";
 import { withTheme } from "theming";
 
 import * as Icons from "./icons";
+import * as DeprecatedComponents from "./_deprecated";
 
 export { default as Button } from "./Button";
 export { default as CarrierLogo } from "./CarrierLogo";
 export { default as Heading } from "./Heading";
 export { default as Text } from "./Text";
 export { default as Icon } from "./Icon";
-export { default as InputText } from "./InputText";
-export { default as InputTextarea } from "./InputTextarea";
-export { default as Checkbox } from "./Checkbox";
-export { default as Header } from "./Container/Header";
-export { default as Section } from "./Container/Section";
-export { default as Select } from "./Select";
-export { default as SystemMessage } from "./SystemMessage";
-export { default as FieldFeedback } from "./FieldFeedback";
 export { default as TextLink } from "./TextLink";
-export { default as Typography } from "./Typography";
-export { default as Radio } from "./Radio";
 export { default as ThemeProvider } from "./Theming/ThemeProvider";
-export { Icons, defaultTheme, withTheme };
+export { Icons, DeprecatedComponents, defaultTheme, withTheme };
