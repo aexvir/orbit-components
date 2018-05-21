e37108a4d69065792c6323bf090f869df6f5c991
Tomáš Hapl <tomas.hapl@gmail.com>
2018-05-21 11:33:44 +0200

    Reverted exports and added deprecation warning

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import defaultTheme from "@kiwicom/orbit-design-tokens";
 import { withTheme } from "theming";
 
 import * as Icons from "./icons";
-import * as DeprecatedComponents from "./_deprecated";
+
+// Deprecated
+export { default as InputTextarea } from "./_deprecated/InputTextarea";
+export { default as Checkbox } from "./_deprecated/Checkbox";
+export { default as Header } from "./_deprecated/Container/Header";
+export { default as Section } from "./_deprecated/Container/Section";
+export { default as Select } from "./_deprecated/Select";
+export { default as SystemMessage } from "./_deprecated/SystemMessage";
+export { default as FieldFeedback } from "./_deprecated/FieldFeedback";
+export { default as Typography } from "./_deprecated/Typography";
+export { default as Radio } from "./_deprecated/Radio";
 
 export { default as Button } from "./Button";
 export { default as CarrierLogo } from "./CarrierLogo";
@@ -12,4 +22,4 @@ export { default as Text } from "./Text";
 export { default as Icon } from "./Icon";
 export { default as TextLink } from "./TextLink";
 export { default as ThemeProvider } from "./Theming/ThemeProvider";
-export { Icons, DeprecatedComponents, defaultTheme, withTheme };
+export { Icons, defaultTheme, withTheme };
